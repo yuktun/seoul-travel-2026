2590592b957007a999a0b79b91587596366a978a
Nixon <yuktun@gmail.com>
2026-08-12 22:51:05 +0800

    Fix standalone viewport safe area layout

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='seoul-travel-v1.22';
-const ASSETS=['./','./index.html','./styles.css?v=1.22','./app.js?v=1.22','./manifest.webmanifest?v=1.22','./app-icon-192.png','./app-icon-512.png','./apple-touch-icon.png'];
+const CACHE='seoul-travel-v1.23';
+const ASSETS=['./','./index.html','./styles.css?v=1.23','./app.js?v=1.23','./manifest.webmanifest?v=1.23','./app-icon-192.png','./app-icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
